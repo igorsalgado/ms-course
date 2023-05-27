d454f1089c26f2ce041f6980ef787907c715a4ba
igorsalgado <salgado.igor.is@gmail.com>
2023-05-27 12:31:58 -0300

    feat: Implementando endpoints

---
+++ hr-worker/src/main/java/com/dev/hrworker/controller/WorkerController.java
@@ -0,0 +1,39 @@
+package com.dev.hrworker.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.hrworker.entities.Worker;
+import com.dev.hrworker.repositories.WorkerRepository;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerController {
+	
+	@Autowired
+	private WorkerRepository workerRepository;
+	
+	
+	@GetMapping
+	public ResponseEntity<List<Worker>> findAll() {
+		List<Worker> workerList = workerRepository.findAll();
+		
+		return ResponseEntity.ok(workerList);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		Worker workerObj = workerRepository.findById(id).get();
+		
+		return ResponseEntity.ok(workerObj);
+	}
+	
+	
+
+}
---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Alex', 100.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Bob', 50.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Maria', 60.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Anna', 120.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('John', 80.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Mark', 110.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Phil', 90.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Peter', 130.0);
